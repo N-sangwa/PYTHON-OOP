589310efa5c8f4a7a1a8eeac0393bc9c17e80f53
N-sangwa <nadiinesangwa@gmail.com>
2022-06-19 20:10:15 +0300

    Deposit and withdraw details, borrow and transfer methods

--- account.py
+++ account.py
@@ -1,4 +1,5 @@
 from unicodedata import name
+from datetime import datetime
 
 
 class Account:
@@ -9,37 +10,119 @@ class Account:
         self.acc_number = acc_number
         self.deposits = []
         self.withdrawals = []
+        self.loan = 0
+        
+        # self.time = datetime.now().strftime("%x %X")
+
     def deposit (self,amount):
         if amount <=0:
             return f"Deposit amount should be more than zero"
 
         else:    
             self.balance += amount
-            self.deposits.append(f"You have deposited {amount}")
+          
+            deposits_dictionary= {
+                "date":datetime.now().strftime("%x %X"),
+                "amount" : amount,
+                "narration" : f"You have deposited {amount}. Your balance is {self.balance}"
+            }
+            self.deposits.append(deposits_dictionary)
             return f"You have deposited {amount}.Your new balance is {self.balance}"
            
     def withdraw(self,amount):
 
-        if amount > self.balance:
-            return "Yout balance is {self.balance} you can not withdraw {amount}"
+        if amount + self.transaction > self.balance:
+            return "Your balance is {self.balance} you can not withdraw {amount}"
         elif amount <= 0:
             return f"Amount must be greater than zero"
         else:
             self.balance -= amount + self.transaction
-            self.withdrawals.append (f"You have withrawn {amount}")
-            return f"You have withdrawn{amount} your balance is {self.balance}"
+            withdrawal_dictionary = {
+
+                "date": datetime.now().strftime("%x %X"),
+                "amount": amount,
+                "narration": f"You have withdrawn {amount} your balance is {self.balance}"
+
+            }
+            self.withdrawals.append(withdrawal_dictionary)
+            return f"You have withdrawn {amount} your balance is {self.balance}"
+        
+
     def deposits_statement(self):
         for statements in self.deposits:
-            print(statements)
+            print(statements["narration"])
+
     def withdraws_statement(self):
         for staments in self.withdraws:
-            print(staments)
+            print(staments["narration"])
+
     def current_balance(self):
         balance = self.balance
-        print(balance)
+        return f"Your current balance is {balance}"
 
+    def full_statements(self):
+        full_statements_list = self.deposits + self.withdrawals
+        # sorted_full_statements_list =sorted(full_statements_list)  
+        for statement in full_statements_list:
+            if statement in self.deposits:
+                print (f"{statement['date']}___Deposit___{ statement['amount']}")
+        for statement in full_statements_list:
+            if statement in self.withdrawals:
+                print (f"{statement['date']}___Withdraw___{ statement['amount']}")
 
-   
+    def borrow(self,amount):
+        deposit_sum=0
+        for a in self.deposits:
+            deposit_sum+=a["amount"]
+        if amount<=0:
+            return "Dear customer, you can not borrow 0"
+        elif len(self.deposits)<10:
+            return f"You need to do at least 10 deposits to be able to access the loan service, make {10-len(self.deposits)} more deposits to qualify"
+        elif amount<100:
+            return "You can only borrow at least 100"
+        elif self.balance!=0:
+            return f"Your balance is {self.balance},you must have 0 balance to be eligible to this service"
+        elif amount > deposit_sum/3:
+            return f"You are not qualified to borrow this amount. You can borrow at most {deposit_sum/3}"
+        elif self.loan!=0:
+            return f"YOu have unpaid loan of {self.loan}, for you to borrow first clear the loan you have"
+        else:
+            interest=(3/100)*amount
+            self.loan+= amount + interest
+            return f"You have borrowed {amount} and your will pay{self.loan}"
+
+    def loan_repayment(self,amount):
+        if amount<=0:
+            return "Dear customer, You can not pay 0 amount, surely?"
+        if amount>self.loan:
+            remainder=amount-self.loan
+            self.loan=0
+            return f"Your loan balance is {self.loan} { self.deposit(remainder)}"
+        else:
+            self.loan-=amount
+            return f"You have paid a loan of {amount} KSH and your current loan balance is {self.loan}"
+
+    def transfer(self,amount,instance_name):
+        if amount<=0:
+            return "invalid amount"
+        elif amount>=self.balance:
+            return "insufficient amount"
+        else:
+            self.balance-=amount
+            instance_name.balance+=amount
+            return f"You have transfered {amount} KSH to {instance_name} account with the name of {instance_name.account_name}. Your new balance is {self.balance}"
+            
+    
+                
+            
+        
+
+       
+            
+           
+        
+
+        
 
 
 
